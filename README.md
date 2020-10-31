2aa15775ab9c739152ca179551e651ed8b3f8ad4
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-10-31 01:30:16 -0600

    Five day forecast programmed, code commented, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # weather-dashboard
+
+## Overview:
+A weather app that displays a city's current weather, its 5-day forecast,
+and your search history for the session.
+
+
+## HTML:
+Most content inside Bootstrap cards to keep things aligned and responsive
+
+I had trouble keeping the 5-day forecast aligned, so I turned it into a Bootstrap
+table, which is highly responsive and scrolls easily on smaller viewports
+
+
+## CSS:
+Very light CSS work, only changing limited background colors and other small adjustments
+
+
+## JS:
+Location data was obtained through the MapQuest API
+
+Weather data through the OpenWeather API
+
+I used one function to look up a city's coordinates and then obtain the weather from those coords.
+This was done because various buttons/elements needed to be able to populate weather data on the page, and I didn't want to repeat myself at all. Function is defined once and used extensively throughout.
+
+
+## Screencaps:
+
+![Page has been reloaded and Boise's weather reappears, since it was last city searched.](screencaps/cap-1.png);
+
+![Search history is shown building up here](screencaps/cap-2.png);
+
+![5-day forecast table shown here](screencaps/cap-3.png);
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-// Main DOM Vars
+// Main DOM/Vars
 var btn = $("button");
 var searchTerms;
 var searchHist = [];
-// MapQuest API vars
+var recents = $("#recents");
+var inputField = $("#input-field");
+var d = new Date();
+var today = d.getDate();
+// MapQuest DOM and API vars
 var lat;
 var long;
 var city;
 var cityEl = $("#city-el");
-// OpenWeather API vars
+// OpenWeather DOM and API vars
 var date;
 var dateEl = $("#date-el");
+var iconEl = $("#icon-el");
+var forecastDates = $("#forecast-dates");
+var iconRow = $("#icon-row");
+var tempRow = $("#temp-row");
+var humRow = $("#hum-row");
 var temp;
 var hum;
 var wind;
@@ -18,13 +27,15 @@ var tempEl = $("#temp-el");
 var humEl = $("#hum-el");
 var windEl = $("#wind-el");
 var uvColorEl = $("#color-code");
+var lastSearch = localStorage.getItem("history");
 
 btn.on('click', function (event) {
     event.preventDefault();
 
+    // Whatever was typed is saved in array for search history
     searchTerms = $("input").val();
     searchHist.push(searchTerms);
-    $("#recents").empty();
+    recents.empty();
 
     for (i = 0; i < searchHist.length; i++) {
 
@@ -32,23 +43,30 @@ btn.on('click', function (event) {
         newCard.addClass("card");
         newCard.attr({
             style: "padding: 10px; background-color: rgba(0,0,0,.03); margin-top: 5px;",
+            // Each of these search history cards will call the normal weather function
             onclick: "histSearch(event)"
         });
         newCard.text(searchHist[i]);
-        $("#recents").append(newCard)
+        recents.prepend(newCard)
     };
     
     populateData();
 
 });
 
+// Same main function used to populate all fields; this function is used repeatedly on different buttons/elements
 function populateData () {
     var urlSearch = searchTerms.trim();
+    inputField.val("");
 
     tempEl.empty();
     humEl.empty();
     windEl.empty();
     uvColorEl.empty();
+    forecastDates.empty();
+    iconRow.empty();
+    tempRow.empty();
+    humRow.empty();
 
     $.ajax({
         url: "http://www.mapquestapi.com/geocoding/v1/address?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + urlSearch,
@@ -58,17 +76,20 @@ function populateData () {
         long = response.results[0].locations[0].latLng.lng;
         city = response.results[0].locations[0].adminArea5;
         cityEl.text(city);
+        dateEl.text((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear())
         
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=38620143f0feedbee7c963b8f6742cad",
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             tempEl.append(" " + response.current.temp.toFixed(0) + "° F");
             humEl.append(" " + response.current.humidity + "%");
             windEl.append(" " + response.current.wind_speed.toFixed(1) + " mph");
             uvColorEl.append(response.current.uvi.toFixed(1));
+            // The @2x at the end here is optional, uses bigger icon, very useful for higher-res icons
+            iconEl.attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png")
             
+            // This sets the color of the UV index based on intensity
             if (response.current.uvi.toFixed(1) < 6) {
                 uvColorEl.attr("style", "background-color: yellow;")
             } else if (response.current.uvi.toFixed(1) < 8) {
@@ -76,6 +97,30 @@ function populateData () {
             } else {
                 uvColorEl.attr("style", "background-color: red;")
             };
+
+            for (i = 1; i < 6; i++) {
+                // This method of setting the date makes sure that all dates and months are updated automatically,
+                // eg. it makes sure that there is no February 30th ever,
+                // that Dec 31st + 1 = Jan 1st, etc.
+                d.setDate(d.getDate() + 1);
+                var newHead = $("<th>");
+                var icon = $("<img>");
+                var iconTD = $("<td>");
+                var tempTD = $("<td>");
+                var humTD = $("<td>");
+                newHead.text((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear());
+                forecastDates.append(newHead);
+                icon.attr("src", "http://openweathermap.org/img/wn/" + response.daily[i - 1].weather[0].icon + ".png");
+                iconTD.html(icon);
+                iconRow.append(iconTD);
+                tempTD.html(response.daily[i - 1].temp.day.toFixed(0) + "° F");
+                tempRow.append(tempTD);
+                humTD.html("Humidity: " + response.daily[i - 1].humidity + "%");
+                humRow.append(humTD);
+            };
+
+            // The last search is stored locally, used at loadLastSearch
+            localStorage.setItem("history", urlSearch);
             
         });
     });
@@ -83,56 +128,19 @@ function populateData () {
 
 
 function histSearch (event) {
-    var urlSearch = event.target.innerHTML.trim();
+    searchTerms = event.target.innerHTML.trim();
 
-    tempEl.empty();
-    humEl.empty();
-    windEl.empty();
-    uvColorEl.empty();
-
-    $.ajax({
-        url: "http://www.mapquestapi.com/geocoding/v1/address?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + urlSearch,
-        method: "GET"
-    }).then(function (response) {
-        lat = response.results[0].locations[0].latLng.lat;
-        long = response.results[0].locations[0].latLng.lng;
-        city = response.results[0].locations[0].adminArea5;
-        cityEl.text(city);
-        
-        $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=38620143f0feedbee7c963b8f6742cad",
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            tempEl.append(" " + response.current.temp.toFixed(0) + "° F");
-            humEl.append(" " + response.current.humidity + "%");
-            windEl.append(" " + response.current.wind_speed.toFixed(0) + " mph");
-            uvColorEl.append(response.current.uvi.toFixed(1));
-            
-            if (response.current.uvi.toFixed(1) < 6) {
-                uvColorEl.attr("style", "background-color: yellow;")
-            } else if (response.current.uvi.toFixed(1) < 8) {
-                uvColorEl.attr("style", "background-color: orange;")
-            } else {
-                uvColorEl.attr("style", "background-color: red;")
-            };
-            
-        });
-    });
+    populateData();
 };
 
-// // MapQuest API
-// $.ajax({
-//     url: "http://www.mapquestapi.com/geocoding/v1/address?key=jHLf4uATR4fijVkLOmrimhIJE79Xp0kx&location=" + searchTerms,
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-// });
 
-// // OpenWeather API
-// $.ajax({
-//     url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=fc081ad068a0564d02828ff534f06c9",
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-// });
+function loadLastSearch () {
+    if (lastSearch !== null) {
+        // lastSearch pulls up city/state that is currently stored locally
+        searchTerms = lastSearch;
+
+        populateData();
+    }
+};
+
+loadLastSearch();
